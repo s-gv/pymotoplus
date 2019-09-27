d72c9e938f64b11cb574863081def83e49161e90
Sagar Gubbi <sagar.writeme@gmail.com>
2019-09-27 16:50:46 +0530

    Add license info

--- pymotoplus.py
+++ pymotoplus.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2019 Sagar Gubbi. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 import socket
 import time
 import sys
